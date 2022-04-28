473dbf46c0c000a4c1b406defe2e5c68927a6562
Vaibhav Singh <vaibhavsingh1987@hotmail.com>
2022-04-28 09:49:03 +0530

    updated variables files

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  default     = "t2.small"
 }
 
 variable "instance_name" {
